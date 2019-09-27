eb284b29b4dff88b6b7301869018c4ffcf755c27
ratzenboeckjakob <j.ratzenboeck1@students.htl-leonding.ac.at>
2019-09-27 08:13:18 +0200

    final commit

--- src/main/java/at/htl/football/League.java
+++ src/main/java/at/htl/football/League.java
@@ -6,29 +6,31 @@ import java.util.List;
 
 public class League {
 
-    private List<Team> teams = new ArrayList<>();
+    private List<Team> teams = new ArrayList<>(18);
 
     public void addMatchResult(Match first) {
         boolean found = false;
 
-
         if(teams.size() != 0) {
             found = false;
-            boolean isHomeTeam;
 
             for (int i = 0; i < teams.size(); i++) {
+
                 if(teams.get(i).getName().equals(first.getGuestName()) || teams.get(i).getName().equals((first.getHomeName()))) {
                     teams.get(i).addMatch(first);
                     found = true;
-                    isHomeTeam = !teams.get(i).getName().equals(first.getGuestName());
                 }
             }
-            if(!found) {
 
+            if(!found) {
                 Team team1 = new Team(first.getGuestName());
                 teams.add(team1);
                 teams.get(teams.size() - 1).addMatch(first);
+                Team team2 = new Team(first.getHomeName());
+                teams.add(team2);
+                teams.get(teams.size() -1).addMatch(first);
             }
+
         } else {
             Team team1 = new Team(first.getGuestName());
             teams.add(team1);
